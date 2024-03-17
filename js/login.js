15748354a80a9818735f4a162315554b48d6c0d1
Nishimura <sigotoyou.mail00@gmail.com>
2024-03-18 06:48:22 +0900

    first commit

--- js/login.js
+++ js/login.js
@@ -1,8 +1,24 @@
 
+import initializeApp from '../firebase/app';
+import  getFirestore  from "../firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCt711E1QJ59Q3AxiTPfJG3brXZXDaDPmA",
+    authDomain: "javascript-test-5c003.firebaseapp.com",
+    projectId: "javascript-test-5c003",
+    storageBucket: "javascript-test-5c003.appspot.com",
+    messagingSenderId: "769365442499",
+    appId: "1:769365442499:web:8375422eb4c1b9e26a8776"
+  };
+
+  // Firebaseアプリを初期化
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
 
 const onClickSubmitButton = (e) =>{
     e.preventDefault(); // フォームのデフォルトの送信を防止
     alert("送信！");
+
    
 
 };
---
+++ main.js
@@ -0,0 +1,240 @@
+
+
+function Main(input) {
+  const inputArr = input.split("\n");
+  console.log(inputArr);
+
+}
+
+  var input = "4\n10\n8\n8\n6";
+  Main(input);
+
+
+
+
+// function Main(input) {
+//   const number = input.split("\n");
+//   const inputArr = number[1].split(" ");
+//   let maxIndex = 0;
+//   let Alice = 0;
+//   let Bob = 0;
+//   const dataArr = inputArr.map((data)=>{return Number(data);});
+
+//   for(let a = 0 ; number[0]/2 > a ; a++){
+//   maxIndex = dataArr.indexOf(Math.max(...dataArr));
+//   Alice += dataArr[maxIndex];
+//   dataArr.splice(maxIndex,1);
+//   if(dataArr.length !== 0){
+//   maxIndex = dataArr.indexOf(Math.max(...dataArr));
+//   Bob += dataArr[maxIndex];
+//   dataArr.splice(maxIndex,1);
+// }
+// }
+
+// console.log(Alice-Bob);
+
+// }
+
+//   var input = "4\n20 18 2 18";
+//   Main(input);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Main(input) {
+//   const inputArr = input.split(" ");
+//   let count = 0;
+//   let num = 0;
+//   let sumdata = 0;
+
+//   for(let a = 0; a<=inputArr[0];a++){
+//     let intArr = String(a).split("");
+//     sumdata = a;
+//     num = 0;
+//     intArr.forEach(data => {
+//       num = Number(data) + num;
+//     });
+//     // console.log(num);
+//     if(num >=  inputArr[1]&& num <=inputArr[2]){
+//       count += sumdata;
+//     }
+//   }
+//   console.log(count);
+// }
+
+//   var input = "100 4 16";
+//   Main(input);
+
+
+
+
+
+
+
+
+
+
+
+// function Main(input) {
+//   const inputArr = input.split("\n");
+//   const data = parseInt(inputArr[3]);
+//   let count = 0;
+
+//   for(let a = 0; a <= inputArr[0];a++){// 500円玉の計算
+//     for(let b = 0; b <= inputArr[1];b++){// 100円玉の計算
+//         for(let c = 0; c <= inputArr[2];c++){// 50円玉の計算
+//                 if(data===50*c+100*b+500*a){
+//                 count++;
+//                 }
+//             }  
+//         }
+//     }
+
+//   console.log(count);
+// }
+
+//   var input = "2\n2\n2\n100";
+//   Main(input);
+
+
+
+
+
+
+
+
+// function Main(input) {
+//   const inputArr = input.split("\n");
+//   const data = parseInt(inputArr[3]);
+//   let count = 0;
+
+//   for(let a = 0; a <= inputArr[0];a++){// 500円玉の計算
+//     for(let b = 0; b <= inputArr[1];b++){// 100円玉の計算
+//         for(let c = 0; c <= inputArr[2];c++){// 50円玉の計算
+//                 if(data===50*c+100*b+500*a){
+//                 count++;
+//                 }
+//             }  
+//         }
+//     }
+
+//   console.log(count);
+// }
+
+//   var input = "2\n2\n2\n100";
+//   Main(input);
+
+
+// function Main(input) {
+//   let inputdata = input.split("\n")
+//   let arr = inputdata[1].split(" ");
+//   let check = true;
+//   let count = -1;
+
+// while(check){
+//   arr=arr.map((data)=>{
+//     if(data%2===0){
+//     return data/2
+//   }else{
+//     check = false;
+//   }
+//   });
+//   count++
+
+// }
+//   console.log(count);
+// }
+
+//   var input = "3\n5 6 8 10";
+//   Main(input);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Main(input) {
+//   let arr = input[1].split(" ");
+//   let check = true;
+//   let count = -1;
+
+//   do {
+//     arr.forEach((data,index) => {
+//       if(data%2===0){
+//         arr[index]=data/2;
+//       }else{
+//         check = false;
+//       }
+//     });
+//     count++;
+//   } while (check);
+
+// console.log(count);
+
+// }
+
+//   var input = ["3","382253568 723152896 37802240 379425024 404894720 471526144"];
+//   Main(input);
+
+// function Main(input) {
+//   const arr = input.split("");
+//   let count = 0;
+//   arr.forEach(function(data) {
+//     if(data==="1"){
+//       count +=1;
+//     }
+//   });
+//   console.log(count);
+//   }
+
+//   // Main(require("fs").readFileSync("/dev/stdin", "utf8")); // これは必ず必要な呪文です
+
+//   var input = ["011"]
+//   Main(input)
+ 
+
+
+// function Main(input) {
+// 	const data = ( input[0].match( /1/g ) || [] ).length;
+//   console.log(data);
+// }
+// //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+// Main(require("fs").readFileSync("/dev/stdin", "utf8")); // これは必ず必要な呪文です
+
+
+
+
+
+
+
+
+// function Main(input) {
+//   var count = ( input[0].match( /1/g ) || [] ).length;
+//   console.log(count);
+//   }
+
+//   Main(require("fs").readFileSync("/dev/stdin", "utf8")); // これは必ず必要な呪文です
+
+//   var input = ["000"]
+//   main(input)
+ 
+  // main(require('fs').readFileSync('/dev/stdin', 'utf8'))
